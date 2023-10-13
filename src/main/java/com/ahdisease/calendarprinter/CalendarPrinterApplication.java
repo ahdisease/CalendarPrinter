e0c97d75fb50cae7a8f1101cc7ce829ccb21a7d2
Alexander Stuart <ahdisease@gmail.com>
2023-10-12 23:49:04 -0400

    created project

---
+++ src/main/java/com/ahdisease/calendarprinter/CalendarPrinterApplication.java
@@ -0,0 +1,13 @@
+package com.ahdisease.calendarprinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarPrinterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarPrinterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahdisease/calendarprinter/CalendarPrinterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahdisease.calendarprinter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarPrinterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
