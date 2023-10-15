6bb097f3d38be71a0e26ffc93a621863ecc8e5bc
Alexander Stuart <ahdisease@gmail.com>
2023-10-15 13:06:25 -0400

    started CalendarEvent model object and testing

---
+++ src/main/java/com/ahdisease/calendarprinter/model/CalendarEvent.java
@@ -0,0 +1,59 @@
+package com.ahdisease.calendarprinter.model;
+
+import org.springframework.format.datetime.DateFormatter;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+public class CalendarEvent {
+    /*
+    SUMMARY:Abraham Lincoln
+    UID:c7614cff-3549-4a00-9152-d25cc1fe077d
+    SEQUENCE:0
+    STATUS:CONFIRMED
+    TRANSP:TRANSPARENT
+    RRULE:FREQ=YEARLY;INTERVAL=1;BYMONTH=2;BYMONTHDAY=12
+    DTSTART:20080212
+    DTEND:20080213
+    DTSTAMP:20150421T141403
+    CATEGORIES:U.S. Presidents,Civil War People
+    LOCATION:Hodgenville\, Kentucky
+    GEO:37.5739497;-85.7399606
+    DESCRIPTION:Born February 12\, 1809\nSixteenth President (1861-1865)\n\n\n
+     \nhttp://AmericanHistoryCalendar.com
+    URL:http://americanhistorycalendar.com/peoplecalendar/1,328-abraham-lincol
+ n
+     */
+    //constants
+    private final String PATTERN_FORMAT = "yyyyMMdd'T'HHmmssz";
+
+    //instance variables
+    private String summary;
+    private UUID uuid;
+    private ZonedDateTime startDate;
+
+    public CalendarEvent(String summary, ZonedDateTime startDate) {
+        uuid = UUID.randomUUID();
+        this.summary = summary;
+        this.startDate = startDate;
+    }
+
+    private String DateToUTCString(ZonedDateTime date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(PATTERN_FORMAT)
+                .withZone(ZoneId.of("Z"));
+        return formatter.format(date);
+    }
+    @Override
+    public String toString() {
+        StringBuilder eventText= new StringBuilder("BEGIN:VEVENT");
+        eventText.append("\nSUMMARY:" + summary );
+        eventText.append("\nUID:" + uuid);
+        eventText.append("\nDATEST:" + DateToUTCString(startDate));
+
+        eventText.append("\nEND:VEVENT");
+        return eventText.toString();
+    }
+}
---
+++ src/test/java/com/ahdisease/calendarprinter/model/CalendarEventTests.java
@@ -0,0 +1,46 @@
+package com.ahdisease.calendarprinter.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Locale;
+
+public class CalendarEventTests {
+
+    @Test
+    public void toString_returns_expected_value() {
+        //arrange
+            //create expected calendar event string
+        String[] expectedICalendarLines = new String[] {
+                "BEGIN:VEVENT",
+                "SUMMARY:Spring Begins",
+                "UID:",
+                "DATEST:20230321T050000Z",
+                "END:VEVENT"
+        };
+
+            //create calendar event with
+        ZonedDateTime dateTime = ZonedDateTime.of(2023 , 03, 21, 0, 0, 0,0, ZoneId.of("EST",ZoneId.SHORT_IDS));
+        CalendarEvent firstDayOfSpring = new CalendarEvent("Spring Begins", dateTime);
+
+        //act
+        String iCalendarString = firstDayOfSpring.toString();
+        String[] iCalendarLines = iCalendarString.split("\n");
+
+        //assert
+        Assertions.assertEquals(expectedICalendarLines.length, iCalendarLines.length);
+
+        for (int i = 0; i < iCalendarLines.length; i++) {
+            if(iCalendarLines[i].startsWith("UID")) {
+                //todo implement regex checking
+                continue;
+            }
+
+            Assertions.assertEquals(expectedICalendarLines[i],iCalendarLines[i]);
+        }
+    }
+
+}
