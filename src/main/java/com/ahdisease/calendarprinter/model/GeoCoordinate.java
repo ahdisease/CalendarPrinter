031b224971b85fa96b2562b38e3f9c3fd446614a
Alexander Stuart <ahdisease@gmail.com>
2023-11-08 23:01:53 -0500

    Created GeoCoordinate class for GEO property

---
+++ src/main/java/com/ahdisease/calendarprinter/model/GeoCoordinate.java
@@ -0,0 +1,24 @@
+package com.ahdisease.calendarprinter.model;
+
+public class GeoCoordinate {
+    private final double latitude;
+    private final double longitude;
+
+    public GeoCoordinate(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String toString() {
+        return String.format( "%.6f;%.6f",latitude,longitude);
+    }
+}
